76c137942b3d3d7d1da9e1799fd17aa1dbbf9f26
triveni <40486032+miffy1234@users.noreply.github.com>
2018-07-30 16:00:27 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# myfirstreactapp
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+// import logo from './logo.svg';
+// replace the original logo from the generated app with one we'll supply
+import logo from "./logo2.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
